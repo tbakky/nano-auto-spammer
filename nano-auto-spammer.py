e075d586b490272935908c93e8dac2798e80df3a
tbakky <48482119+tbakky@users.noreply.github.com>
2019-03-12 09:57:32 -0400

    Update nano-auto-spammer.py

--- nano-auto-spammer.py
+++ nano-auto-spammer.py
@@ -1,21 +1,18 @@
 from nanocurrency import *
-from time import sleep
 import requests
 import sys
 
 def get_account_info():
-    if len(sys.argv) < 5:
+    if len(sys.argv) < 6:
         print("NOT ENOUGH ARGS")
         exit(0)
 
     account_1 = sys.argv[1]
     seed_1 = sys.argv[2]
-    key_1 = generate_account_private_key(seed_1, 0)
     account_2 = sys.argv[3]
     seed_2 = sys.argv[4]
-    key_2 = generate_account_private_key(seed_2, 0)
     wallet = sys.argv[5]
-    return account_1, seed_1, key_1, account_2, seed_2, key_2, wallet
+    return account_1, seed_1, account_2, seed_2, wallet
 
 def send(wallet, source, destination, amount):
     r = requests.post(
@@ -27,7 +24,7 @@ def send(wallet, source, destination, amount):
     return r.json()["block"]
 
 def main():
-    account_1, seed_1, key_1, account_2, seed_2, key_2, wallet = \
+    account_1, seed_1, account_2, seed_2, wallet = \
         get_account_info()
 
     amount = 1000000
